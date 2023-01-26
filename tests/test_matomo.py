b7d1e9b5633eb37cfaccb87666a15145b04d1ebe
Arjan Mossel <arjanmossel@gmail.com>
2023-01-26 11:58:40 +0100

    Add test for plugin configuration

--- tests/test_matomo.py
+++ tests/test_matomo.py
@@ -9,3 +9,12 @@ async def test_plugin_is_installed():
     assert response.status_code == 200
     installed_plugins = {p["name"] for p in response.json()}
     assert "datasette-matomo" in installed_plugins
+
+
+@pytest.mark.asyncio
+async def test_plugin_is_configured(monkeypatch):
+    monkeypatch.setenv("MATOMO_SERVER_URL", "https://example.com/")
+    monkeypatch.setenv("MATOMO_SITE_ID", "1")
+    datasette = Datasette(memory=True)
+    response = await datasette.client.get("/")
+    assert '<script src="/-/matomo-tracking.js"' in response.text
